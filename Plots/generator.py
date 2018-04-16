491ff4528930793fe9a13e986ed10211e61b6907
INFJakZda <zdanowski78@gmail.com>
2018-04-16 22:19:40 +0200

    Remove polish comments & clear code - PLOTS

--- Plots/generator.py
+++ Plots/generator.py
@@ -1,88 +1,112 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-import statistics
-import matplotlib.pyplot as plt
 import csv
+import matplotlib.pyplot as plt
+import numpy
+
+filesToRead = ['rsel.csv', 'cel-rs.csv', '2cel-rs.csv', 'cel.csv', '2cel.csv']
+namesToChars = ['1-Evol-Rs', '1-Coev-RS', '2-Coev-RS', '1-Coev', '2-Coev']
 
 def main():
-    names = ['rsel.csv','cel-rs.csv', '2cel-rs.csv', 'cel.csv', '2cel.csv']     #pomocnicza lista nazw plikow z danymi
-    plots = plt.figure(figsize=(9, 6.7))                                        #ustawienie rozmiaru obrazka
-    lower = plots.add_subplot(121)                                              #ustawienie dwoch obrazkow obok siebie
-    #PLOT
-    for name in names:                  #dla każdej nazwy pliku
-        flag = 0                        #pomijanie pierwszej linijki w csv
-        osX = []                        #wartosci na osi X
-        osY = []                        #wartosci na osi Y
-        suma = []                       #pomocnicza lista dla osY     
-        with open("data/" + name, 'r') as plik:           #otwieranie kazdego pliku z danymi
-            dane = csv.reader(plik)             #czytanie konkretnego pliku
-            for row in dane:                    #rozkladanie danych wiersz po wierszu
-                if flag == 0:                   #pomijanie pierwszej linijki w csv - niepotrzebne nazwy kolumn
-                    flag = 1                    #otwarcie bramy po minięciu pierwszego wiersza w pliku
-                    continue                    #przejscie do drugiego wiersza 
-                osX.append(int(row[1]) / 1000)  #druga kolumna( [1] ) to os X, odpowiednio przeskalowana
-                for i in range(2,34):           #odczytanie wszytskich wartosci w wierszu
-                    suma.append(float(row[i]))  #do pomocniczej listy dodajemy kolejne wartosci  
-                osY.append(statistics.mean(suma) * 100)     #aby teraz policzyc jej srednia i wpisac do osi Y                
-                suma = []                       #czyszczenie pomocniczej listy
-            #dodanie osi X, Y,   nazwy lini,        koloru        znacznikow  rozmiaru itd     
-        if name == 'rsel.csv':
-            lower.plot(osX, osY, label="1-Evol-RS", color='blue', marker='o', markersize=7, markevery=25, markeredgecolor = 'black')
-        if name == 'cel-rs.csv':
-            lower.plot(osX, osY, label="1-Coev-RS", color='green', marker='v', markersize=7, markevery=25, markeredgecolor = 'black')      
-        if name == '2cel-rs.csv':  
-            lower.plot(osX, osY, label="2-Coev-RS", color='red', marker='D', markersize=7, markevery=25, markeredgecolor = 'black')  
-        if name == 'cel.csv':      
-            lower.plot(osX, osY, label="1-Coev", color='black',marker='s', markersize=7, markevery=25, markeredgecolor = 'black')        
-        if name == '2cel.csv':
-            lower.plot(osX, osY, label="2-Coev", color='magenta', marker='d', markersize=7, markevery=25, markeredgecolor = 'black')  
-    #ustawienie parametrow              
-    lower.set_xlabel("Rozegranych gier (x1000)")
-    lower.set_ylabel("Odsetek wygranych gier [%]")
-    lower.set_xlim(0, 500)
-    lower.set_ylim(60, 100)
-
-    top = plt.twiny()
-    top.set_xlim(0, 200)
-    top.set_xticks([0, 100, 200, 300, 400, 500])                #wyskalowanie osi X aby wartosci na dolnej i gornej osi byly zgodne
-    top.set_xticklabels(["0", "40", "80", "120", "160", "200"])
-    top.set_xlabel("Pokolenie")
-
-    top.tick_params(direction = "in")
-    lower.tick_params(direction = "in")
-    lower.legend()
-    lower.grid(color='tab:gray', linestyle=':')
+    avgF = lambda List: float(sum(List)) / max(len(List), 1)
+    dataDiction = {}
+    markerSet = ['o', 'v', 'D', 's', 'd']
+    lineColor = ['#0385F7', '#179B03', '#B70303', 'black', '#BF00CD']
     
-    #BOX PLOT 
-    box_list = []                       #lista z danymi z ostatniego iwersza osi X
-    box_mean = []                       #lista ze srednimi do box plota
-    box = plots.add_subplot(122)        
-    for name in names:
-        with open("data/" + name, 'r') as plik:
-            help_list = []                  #lista pomocnicza 
-            dane = list(csv.reader(plik))   
-            last = dane[-1]                 #odczyt ostatniej linijki z csv
-            for wart in last:
-                help_list.append(float(wart) * 100.0)   #wprowadzenie wyskalowanych wartosci do help_list
-            box_list.append(help_list[2:])              
-            box_mean.append(sum(help_list[2:]) / len(help_list[2:]))
-    bx = box.boxplot(box_list, notch=True, flierprops = dict(marker = '+', markersize = 7, markerfacecolor = 'b', 
-                              markeredgecolor = 'b'), medianprops = dict(color = 'red'))
-    #kosmetyczne zmiany kolor, styl....
-    box.grid(color='tab:gray', linestyle=':')
-    plt.setp(bx['boxes'], color='blue')
-    for sth in bx['whiskers']:
-        sth.set(color='#2B00FF',lw=1)
-        sth.set_linestyle('--')
-    #podanie nazw pod wykresem 
-    box_labels = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]
-    box.set_xticklabels(box_labels, rotation=20)
-    box.yaxis.tick_right()
-    box.scatter([1, 2, 3, 4, 5], box_mean)
-    box.set_ylim(60, 100)
-    plt.tick_params(direction = "in")
-    #koncowe operacje na plt
-    plt.savefig('out/my.finish.plot.pdf')
+    # Load data from csv files
+    for file in filesToRead:
+        with open("data/" + file, 'r') as f:
+            readCsv = csv.reader(f)
+            i = 0
+            dataListHelperTwo = []
+            for row in readCsv:
+                # This two if block is to remove first trash info
+                if i == 0:
+                    i += 1
+                elif i == 1:
+                    dataListHelperOne = []
+                    dataListHelperOne.append(row[0])
+                    dataListHelperOne.append(row[1])
+                    tempList = []
+                    
+                    for x in range(2, 34):
+                        tempList.append(float(row[x]))
+                    
+                    dataListHelperOne.append(avgF(tempList))
+                    dataListHelperTwo.append(dataListHelperOne)
+                    
+            dataDiction[file] = dataListHelperTwo
+    # ********************************************************************************
+    # CHAR PLOT
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax2 = ax1.twiny()
+        
+    for ftr in range(0, 5):
+        x = []
+        y = []
+        marker_style = dict(marker = markerSet[ftr], markersize = 4, markeredgecolor = 'black', 
+                            markerfacecoloralt = lineColor[ftr], markevery = 25)
+        for i in range(0, 200):
+            x.append(int(dataDiction[filesToRead[ftr]][i][1])/1000)
+            y.append(dataDiction[filesToRead[ftr]][i][2]*100)
+
+        ax1.plot(x,y, color = lineColor[ftr], linewidth = 0.6, label = namesToChars[ftr], 
+                 linestyle = '-', **marker_style)
+    
+    ax1.grid(b=True, which='major', color='tab:gray', linestyle=':')
+    
+    ax1.set_ylabel('Odsetek wygranych gier [%]')
+    ax1.set_xlabel('Rozegranych gier (x1000)')
+    ax1.set_xlim(0,500)    
+    ax1.set_ylim(60,100)
+    ax1_ticks = ax1.get_xticks()
+    ax2.set_xlim(0,200)
+    ax2_scale = ax1_ticks / 2.5    
+    ax2.set_xticks(ax2_scale)
+    ax2.set_xlabel('Pokolenie') 
+    
+    legend = ax1.legend(loc='lower right', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+
+    for label in legend.get_texts():
+        label.set_fontsize('small')
+
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)
+        
+    #  *****************************************************************************
+    # CHAR BOXPOLT
+    dataDiction = []
+    for file in filesToRead:
+        listHelp = []
+        with open("data/" + file, 'r') as f:
+            lastrow = None
+            for lastrow in csv.reader(f):
+                pass
+            for x in range(2, 34):
+                listHelp.append(float(lastrow[x]) * 100)
+            dataDiction.append(listHelp)      
+    ax1 = fig.add_subplot(122)
+    ax1.yaxis.tick_right()
+    for tick in ax1.get_xticklabels():
+        tick.set_rotation(25)
+    ax1.set_ylim(60,100)
+    flierprops = dict(marker = '+', markerfacecolor = 'b', markersize = 4,
+                  linestyle = 'none', markeredgecolor = 'b')
+    meanpointprops = dict(marker = 'o', markeredgecolor = 'black', 
+                          markerfacecolor = 'blue', markersize = 4)
+    bp = ax1.boxplot(dataDiction, 1, labels = namesToChars, flierprops = flierprops, 
+                     showmeans = True, meanline = False, meanprops = meanpointprops)
+    ax1.grid(b=True, which='major', color='tab:gray', linestyle=':')
+    plt.setp(bp['boxes'], color='blue')
+    for whisker in bp['whiskers']:
+        whisker.set(color='#2B00FF',lw=1)
+        whisker.set_linestyle('--')
+    
+    plt.savefig('out/test.pdf')
     plt.close()
 
 if __name__ == '__main__':
